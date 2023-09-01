2f4f4a4b59d0fbf8f224c16d29a9adef1a6d72f5
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-08-31 21:49:22 -0300

    corrigindo bug no delete

--- src/repositories/carsRepository.js
+++ src/repositories/carsRepository.js
@@ -182,7 +182,12 @@ export async function saleCancel(id) {
 }
 
 export async function saleDelete(id) {
-    db.query(`DELETE FROM photos WHERE "carId" = $1;`, [id]);
-    db.query(`DELETE FROM favorites WHERE "carId" = $1;`, [id]);
-    return db.query(`DELETE FROM cars WHERE "id" = $1;`, [id]);
+
+    await db.query(`
+        DELETE FROM photos WHERE "carId" = $1;
+        DELETE FROM favorites WHERE "carId" = $1;
+        DELETE FROM cars WHERE "id" = $1;
+    `, [id]);
+
+    return;
 }
